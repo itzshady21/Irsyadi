7f7926ab9d9f4e1f59f68bf6c361e353d2ae9093
Shady <146943557+itzshady21@users.noreply.github.com>
2023-11-29 16:06:41 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Belajar PHP</h1>
+    <?php
+      $a=10;
+      $b=4;
+      $c=14;
+      $d=7;
+      $e=3;
+
+      echo "Hasil dari Tambahan $a + $b = ";
+      echo "$c";
+      echo "</br>";
+      echo "========================";
+      echo "</br>";
+      echo "Hasil dari Pengurangan $d - $b = ";
+      echo "$e";
+      
+    ?>
+</body>
+</html>
